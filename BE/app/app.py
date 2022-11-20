6c6fb03e495a868cacf2b2778a50380849a9c1ac
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-20 21:18:16 +0900

    refactor: routing api

--- BE/app/app.py
+++ BE/app/app.py
@@ -1,30 +1,13 @@
 from fastapi import FastAPI
-from .routes import s3bucket
+from .routes import s3bucket,recommend
 from dotenv import load_dotenv
 import os
 app = FastAPI()
-app.include_router(s3bucket.router,prefix="/images")
+app.include_router(s3bucket.router)
+app.include_router(recommend.router)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR,".env"))
 
-#아래 싹 다 라우팅하기
 @app.get("/")
-async def test():
-    return {"test"}
-@app.get("/app")
 async def main():
-    return {'User_content':[{'photo' : 'photoURL1', 'name' : 'photo_name_1', 'idx': '1'},
-                            {'photo' : 'photoURL2', 'name' : 'photo_name_2', 'idx': '2'},
-                            {'photo' : 'photoURL3', 'name' : 'photo_name_3', 'idx': '3'}]}
-@app.get("/app/{idx}")
-async def into_closet(idx : str):
-    return {
-        'prev_content' : {'photo:':'photo_url'+idx},
-        'User_content':[{'photo' : 'photoURL1',  'idx': '1'},
-                            {'photo' : 'photoURL2',  'idx': '2'},
-                            {'photo' : 'photoURL3', 'idx': '3'}]}
-    
-@app.get("/app/recomandation/{idx}")
-async def into_closet(idx : str):
-    return {
-        'User_content':{'photo' : 'photoURL'+idx,  'info_url': 'https://www.musinsa.com/app/blackfriday/special'}}
+    return {"message": "test api"}
--- BE/app/routes/s3bucket.py
+++ BE/app/routes/s3bucket.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, UploadFile, File
 from ..models.s3service import s3service
 router = APIRouter()
 
-@router.post("/s3", tags=["s3bucket"])
+@router.post("/upload", tags=["s3bucket"])
 async def put_images(file: UploadFile = File(None)):
     s3 = s3service()
     res = await s3.upload_file(file)
