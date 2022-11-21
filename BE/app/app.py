63e58278470e49514f47a4ddb3dea1f10ace8f19
KIM <vkn123@naver.com>
2022-11-21 12:27:40 +0900

    add middleware

--- BE/app/app.py
+++ BE/app/app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
-from .routes import s3bucket
+from routes import s3bucket
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(s3bucket.router)
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def read_root():
